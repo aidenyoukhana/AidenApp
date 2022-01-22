e65c7954a97f9c61e019bdb8917df02e64c41f1a
Aiden Youkhana <81486691+aidenyoukhana@users.noreply.github.com>
2022-01-22 13:08:42 -0700

    Initial Commit

---
+++ Aiden/AidenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AidenApp.swift
+//  Aiden
+//
+//  Created by Aiden.Youkhana on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct AidenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aiden/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Aiden
+//
+//  Created by Aiden.Youkhana on 1/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
